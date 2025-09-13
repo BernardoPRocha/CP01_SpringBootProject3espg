a7dc9f132a0db2413fca6598b7e263c4b867bff8
BernardoPRocha <126582923+BernardoPRocha@users.noreply.github.com>
2025-09-12 22:32:41 -0300

    Update InstrutorController.java

--- CP01_SpringBootProject3espg/src/main/java/br/com/fiap3espg/spring_boot_project/controller/InstrutorController.java
+++ CP01_SpringBootProject3espg/src/main/java/br/com/fiap3espg/spring_boot_project/controller/InstrutorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/instrutor")
+@RequestMapping("/Instrutor")
 public class InstrutorController {
 
     @Autowired
